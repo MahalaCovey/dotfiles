b997a882143ef6e06764333272cf6da03a396ff9
Mahala Covey <mahala.covey@outlook.com>
2026-02-19 21:29:39 -0500

    Adding script

---
+++ copyInstallVundleVim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp ./vimrc ~ # Copies vimrc file to home directory
+
+if [ ! -f ~/.vim/bundle/Vundle.vim ]; then # Installs Vundle if it does not exist
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall # Installs vim setup
